dd330505ab1529988a4dd2a9c66e79908d5afae7
Arjit Tiwari <arjittiwari78@gmail.com>
2025-07-19 17:20:05 +0530

    deployment of my project

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require("ejs-mate") ;
 const path =require("path") ;
 const reviewRouter = require("./routes/review.js") ;
 const session = require("express-session") ;
+const MongoStore = require('connect-mongo') ;
 const flash = require("connect-flash") ;
 const listingRouter = require("./routes/listing.js") ;
 const methodOverride = require("method-override") ;
@@ -13,6 +14,7 @@ const LocalStrategy = require("passport-local") ;
 const User = require("./models/user.js") ; 
 const { register } = require("module");
 const userRouter = require("./routes/user.js") ;
+require('dotenv').config();
 
 app.set("view engine","ejs") ;
 app.set("views",path.join(__dirname,"views")) ;
@@ -20,7 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method")) ;
 app.engine('ejs',ejsMate) ;
 app.use(express.static(path.join(__dirname,"/public"))) ;
-const mongo_URL ="mongodb://127.0.0.1:27017/wanderlust" ;
+// const mongo_URL ="mongodb://127.0.0.1:27017/wanderlust" ;
+const dbUrl = process.env.ATLASDB_URL  ;
 main().then(()=>{
     console.log("connected to DB") ;
 })
@@ -28,11 +31,21 @@ main().then(()=>{
    console.log(err) ;
 })
 async function main() {
-    await mongoose.connect(mongo_URL);
+    await mongoose.connect(dbUrl);
 }
-
+const store = MongoStore.create({
+  mongoUrl : dbUrl ,
+  crypto :{
+     secret: process.env.SECRET
+  },
+  touchAfter : 24*3600,
+});
+store.on("error",()=>{
+  console.log("error in mongo session store ",err) ;
+}) ;
 const sessionOptions ={
-   secret: "mysupersecretcode" ,
+   store ,
+   secret:process.env.SECRET ,
    resave: false ,
    saveUninitialized: true ,
    cookie :{
